3edb02239046b1d78bc0dc60265ec914bcc6e08d
emirez <erenmertirez@gmail.com>
2018-03-20 10:12:06 +0100

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,49 @@
-#define BLYNK_PRINT Serial
-#define LED1 D3
-
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include "Seeed_BME280.h"
 #include <Wire.h>
+#include <math.h>
 
-char auth[] = "07cb1c39fdd34641a256c37e5f273794";
-char ssid[] = "CASSINIguest";
-char pass[] = "Cassini2016!";
+char auth[] = "193a526bfebf47cc869514d7a6dc3c30";
+char ssid[] = "Eren's iPhone";
+char pass[] = "27081992";
+
+#define BLYNK_PRINT Serial
+#define BUT1 D6
+#define LED1 D3
+#define POT1 A0
+#define PIN_UPTIME V1
 
 BlynkTimer timer;
-BME280 bme280;
 
+// LED Port
+BLYNK_WRITE(0)
+{
+    if (param.asInt()) {
+        digitalWrite(LED1, HIGH);
+    } else {
+        digitalWrite(LED1, LOW);
+    }
+}
+// Potentiometer and Button Port
 void myTimerEvent()
 {
-  Blynk.virtualWrite(V0, (int)bme280.getTemperature());
-  Blynk.virtualWrite(V1, bme280.getTemperature());
-  Blynk.virtualWrite(V2, bme280.getHumidity());
-  Blynk.virtualWrite(V3, bme280.getPressure());
+    Blynk.virtualWrite(V1, analogRead(POT1));
+    Blynk.virtualWrite(V2, digitalRead(BUT1));
 }
 
+// Setup Func
 void setup()
 {
-  Serial.begin(9600);
-    if(!bme280.init()){
-        Serial.println("Device error!");
-    }
-  Blynk.begin(auth, ssid, pass);
-  timer.setInterval(1000L, myTimerEvent);
+    Serial.begin(9600);
+    Blynk.begin(auth, ssid, pass);
+    pinMode(LED1, OUTPUT);
+    pinMode(BUT1, INPUT);
+    pinMode(POT1, INPUT);
+    timer.setInterval(1000L, myTimerEvent);
 }
-
+// Loop Func
 void loop()
 {
-  Blynk.run();
-  timer.run();
+    Blynk.run();
+    timer.run();
 }
-
